e51a9c74432f0d113083e09d3a8d344d0d21922e
Burchell <jlkaline@me.com>
2021-03-10 20:40:09 -0500

    get routes

--- server.js
+++ server.js
@@ -25,6 +25,13 @@ const readAsync = util.promisify(fs.readFile);
 let notes;
 
 // API GET Requests
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
 
 // API POST Requests
